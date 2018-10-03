a983291ea76a9308a2322d068ed4a88675c7f6d6
Alex Chheng <alexchheng1998@gmail.com>
2018-10-03 15:05:13 -0700

    Hello Path Finder and with enemy Movement, thanks to the youtube tutorial

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public Transform[] pathPoint;
+    public float enemyMovement;
+
+    private Rigidbody2D rb2d;
+    private int currentPath;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position != pathPoint[currentPath].position)
+        {
+            Vector2 pos = Vector2.MoveTowards(transform.position, pathPoint[currentPath].position, enemyMovement * Time.deltaTime);
+            rb2d.MovePosition(pos);
+        }
+        else
+        {
+            currentPath = (currentPath + 1) % pathPoint.Length;
+        }
+	}
+}
---
+++ Assets/Scripts/PathFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollow : MonoBehaviour {
+
+    public EnemyPathway EnemyPathwayScript;
+    public int currentPathwayID = 0;
+    public float enemySpeed;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+    private float reachDistance = 1.0f;
+
+    Vector2 last_position;
+    Vector2 current_position;
+	// Use this for initialization
+	void Start () {
+        //EnemyPathwayScript = GameObject.Find(pathName).GetComponent<EnemyPathway>();
+        last_position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector2.Distance(EnemyPathwayScript.path_objs[currentPathwayID].position, transform.position);
+        transform.position = Vector2.MoveTowards(transform.position, EnemyPathwayScript.path_objs[currentPathwayID].position, Time.deltaTime * enemySpeed);
+
+        if(distance <= reachDistance)
+        {
+            currentPathwayID++;
+        }
+	}
+}
