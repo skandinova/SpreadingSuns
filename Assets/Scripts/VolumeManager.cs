5f566cd9a2a687f67f0504f8798f84f176a2ba5b
skandinova <jacobrolson14@gmail.com>
2018-10-15 01:22:34 -0700

    Added controls screen, PlayerPrefsManager script, and functionality for volume slider

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour {
+    public Slider volumeSlider;
+
+    private void Start() {
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+    }
+
+    private void Update() {}
+
+    public void SetVolume() {
+        PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+    }
+}
