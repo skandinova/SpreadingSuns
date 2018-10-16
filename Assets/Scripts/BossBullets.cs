0c1d60c142747f5ca729b7cc2c6ec8000ed50348
Alex Chheng <alexchheng1998@gmail.com>
2018-10-16 16:01:27 -0700

    Boss rotation and reverse pattern - Press space to do a reverse rotation effect Enemy stop rest - must go to the waypoint and change the numStop correspond to the object number, otherwise will not be in effect
    Add new scripts

--- Assets/Scripts/BossBullets.cs
+++ Assets/Scripts/BossBullets.cs
@@ -7,22 +7,37 @@ public class BossBullets : MonoBehaviour {
     [Header("Projectile Settings")]
     public int numberOfProjectiles;
     public float projectileSpeed;
+    public float rotationSpeed;
+    public float bulletPerSeconds;
+    public float rotationSave;
     public GameObject projectilePrefab;
+    private bool reverseBulletBool;
+    private bool shotBool;
 
     [Header("Private Variables")]
     private Vector2 startPoint;
     private const float radius = 1F;
 
     void Start () {
-		
+        shotBool = false;
+        reverseBulletBool = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (shotBool == false)
         {
-            startPoint = transform.position;
-            SpawnProjectile(numberOfProjectiles);
+            shotBool = true;
+            StartCoroutine(CoWaitAndDoThing());
+        }
+
+        if (Input.GetKeyDown("space") && reverseBulletBool == false)
+        {
+            reverseBulletBool = true;
+        }
+        else if (Input.GetKeyDown("space") && reverseBulletBool)
+        {
+            reverseBulletBool = false;
         }
     }
 
@@ -30,11 +45,20 @@ public class BossBullets : MonoBehaviour {
     {
         float angleStep = 360f / _numberOfProjectile;
         float angle = 0f;
+        //Used to save rotation location and add into angle for rotation effect
+        if (reverseBulletBool == false)
+        {
+            rotationSave += rotationSpeed;
+        }
+        else if (reverseBulletBool)
+        {
+            rotationSave -= rotationSpeed;
+        }
 
         for (int i = 0; i <= _numberOfProjectile -1; i++)
         {
-            float projectileDirXPosition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
-            float projectileDirYPosition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+            float projectileDirXPosition = startPoint.x + Mathf.Sin(((angle + rotationSave) * Mathf.PI) / 180) * radius;
+            float projectileDirYPosition = startPoint.y + Mathf.Cos(((angle + rotationSave)* Mathf.PI) / 180) * radius;
 
             Vector2 projectileVector = new Vector2(projectileDirXPosition, projectileDirYPosition);
             Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized * projectileSpeed;
@@ -42,6 +66,23 @@ public class BossBullets : MonoBehaviour {
             GameObject tmpObj = Instantiate(projectilePrefab, startPoint, Quaternion.identity);
             tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
             angle += angleStep;
+
+            if (rotationSave >= 360f)
+            {
+                rotationSave -= 360f;
+            }
+            else if (rotationSave <= -360)
+            {
+                rotationSave += 360f;
+            }
         }
+    }
+
+    IEnumerator CoWaitAndDoThing()
+    {
+        yield return new WaitForSeconds(bulletPerSeconds);
+        startPoint = transform.position;
+        SpawnProjectile(numberOfProjectiles);
+        shotBool = false;
     }
 }
---
+++ Assets/Scripts/CoroutineExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineExample : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(CoWaitAndDoThing());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator CoWaitAndDoThing()
+    {
+        Debug.Log("Do Thing A");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Do Thing B");
+        float duration = 5f;
+        float timer = 0f;
+        while(timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            // Stop execution, wait for update, then resume.
+            yield return null;
+        }
+    }
+
+}
