b0ba497b32c68011a4ab25a7aca94bf7c3ddcd86
Alex Chheng <alexchheng1998@gmail.com>
2018-10-18 17:38:27 -0700

    Update scripts and bossBullets change purpose, still accomplish

--- Assets/Scripts/BossBullets.cs
+++ Assets/Scripts/BossBullets.cs
@@ -10,30 +10,15 @@ public class BossBullets : MonoBehaviour {
     public float rotationSpeed;
     public float bulletPerSeconds;
     public GameObject projectilePrefab;
-    private float rotationSave;
-    private bool reverseBulletBool;
-    private bool shotBool;
+    public float rotationSave;
+    public bool reverseBulletBool;
 
     [Header("Private Variables")]
     private Vector2 startPoint;
+    private bool shotBool;
     private const float radius = 1F;
-    public float seconds;
-    public bool stop;
 
     void Start () {
-<<<<<<< HEAD
-        startPoint = transform.position;
-        StartCoroutine(SpawnProjectile(numberOfProjectiles));
-    }
-
-    // Update is called once per frame
-    void Update () {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-       //     startPoint = transform.position;
-         //   SpawnProjectile(numberOfProjectiles);
-        //}
-=======
         shotBool = false;
         reverseBulletBool = false;
 	}
@@ -54,10 +39,9 @@ public class BossBullets : MonoBehaviour {
         {
             reverseBulletBool = false;
         }
->>>>>>> b337113492c09817aba8d64f44f6dedb5b6eec86
     }
 
-    private IEnumerator SpawnProjectile(int _numberOfProjectile)
+    private void SpawnProjectile(int _numberOfProjectile)
     {
         float angleStep = 360f / _numberOfProjectile;
         float angle = 0f;
@@ -71,29 +55,14 @@ public class BossBullets : MonoBehaviour {
             rotationSave -= rotationSpeed;
         }
 
-<<<<<<< HEAD
-        while (!stop) {
-            for (int i = 0; i <= _numberOfProjectile - 1; i++)
-            {
-                float projectileDirXPosition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
-                float projectileDirYPosition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
-=======
         for (int i = 0; i <= _numberOfProjectile -1; i++)
         {
             float projectileDirXPosition = startPoint.x + Mathf.Sin(((angle + rotationSave) * Mathf.PI) / 180) * radius;
             float projectileDirYPosition = startPoint.y + Mathf.Cos(((angle + rotationSave)* Mathf.PI) / 180) * radius;
->>>>>>> b337113492c09817aba8d64f44f6dedb5b6eec86
 
-                Vector2 projectileVector = new Vector2(projectileDirXPosition, projectileDirYPosition);
-                Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized * projectileSpeed;
+            Vector2 projectileVector = new Vector2(projectileDirXPosition, projectileDirYPosition);
+            Vector2 projectileMoveDirection = (projectileVector - startPoint).normalized * projectileSpeed;
 
-<<<<<<< HEAD
-                GameObject tmpObj = Instantiate(projectilePrefab, startPoint, Quaternion.identity);
-                tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
-                angle += angleStep;
-            }
-            yield return new WaitForSeconds(seconds);
-=======
             GameObject tmpObj = Instantiate(projectilePrefab, startPoint, Quaternion.identity);
             tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
             angle += angleStep;
@@ -106,7 +75,6 @@ public class BossBullets : MonoBehaviour {
             {
                 rotationSave += 360f;
             }
->>>>>>> b337113492c09817aba8d64f44f6dedb5b6eec86
         }
     }
 
