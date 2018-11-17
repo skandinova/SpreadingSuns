dc68c27c607b0684b355de9996747bafeaf0f3c8
skandinova <jacobrolson14@gmail.com>
2018-11-17 14:07:36 -0800

    Changes to main menu

---
+++ Assets/Scripts/TextColorButtonHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextColorButtonHandler : MonoBehaviour 
+    {
+
+    public Color[] colorArray;
+
+    public void SetColor(int colorArrayIndex) 
+    {
+        GetComponent<Text>().color = colorArray[colorArrayIndex];
+    }
+}
